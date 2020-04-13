e8a5c5966a39a65f7f27324054cb9b7b25bb153d
ryo0 <ryo@unentai.jp>
2020-04-13 12:44:00 +0900

    EvalML1完成

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,17 @@
-
+import solver.solve
 import tokenizer.tokenizer.tokenize
-import parser.parser.parseSum
+import parser.parser.parseExp
 
 object main extends App {
-//  println(solve(parseSum(tokenize("3 + 5"))._1))
-//  println(solve(parseSum(tokenize("8 - 2 - 3"))._1))
-//  println(solve(parseSum(tokenize("(4 + 5) * (1 - 10) "))._1))
+  println(solve(parseExp(tokenize("3 + 5"))._1))
+  println(solve(parseExp(tokenize("8 - 2 - 3"))._1))
+  println(solve(parseExp(tokenize("(4 + 5) * (1 - 10) "))._1))
+  println(solve(parseExp(tokenize("(4 + 5) > (1 - 10) "))._1))
+  println("--------")
+  println(solve(parseExp(tokenize("if 4 < 5 then 2 + 3 else 8 * 8"))._1))
+  println("--------")
+  println(solve(parseExp(tokenize("3 + if -23 < -2 * 8 then 8 else 2 + 4 "))._1))
+  println("--------")
+  println(solve(parseExp(tokenize("3 + (if -23 < -2 * 8 then 8 else 2) + 4 "))._1))
 
 }
