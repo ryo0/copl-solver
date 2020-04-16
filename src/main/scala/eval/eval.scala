587c25317ba409ec8715fd14de891803d7e92ae1
ryo0 <ryo@unentai.jp>
2020-04-16 20:31:14 +0900

    letのeval

--- src/main/scala/eval/eval.scala
+++ src/main/scala/eval/eval.scala
@@ -2,19 +2,32 @@ package eval
 import parser.ast._
 
 object eval {
-  def eval(exp: Exp): Exp = {
+  def initEval(exp: Exp) : Exp = {
+    eval(exp, List())
+  }
+
+  def eval(exp: Exp, env: List[Map[String, Exp]]): Exp = {
     exp match {
       case IntVal(n) => IntVal(n)
-      case IfExp(condExp, thenExp, elseExp) => {
-        if (eval(condExp).asInstanceOf[BoolVal].value) {
-          eval(thenExp)
+      case Var(n) => env.head(n)
+      case LetExp(variable, valueExp, inExp) => {
+        if (env.isEmpty) {
+          eval(inExp, List(Map(variable.name -> eval(valueExp, env))))
         } else {
-          eval(elseExp)
+          val newMap = env.head ++ Map(variable.name -> eval(valueExp, env))
+          eval(inExp, newMap :: env.tail)
+        }
+      }
+      case IfExp(condExp, thenExp, elseExp) => {
+        if (eval(condExp, env).asInstanceOf[BoolVal].value) {
+          eval(thenExp, env)
+        } else {
+          eval(elseExp, env)
         }
       }
       case InfixExp(left, op, right) =>
-        val leftVal = eval(left)
-        val rightVal = eval(right)
+        val leftVal = eval(left, env)
+        val rightVal = eval(right, env)
         op match {
           case Plus =>
             plus(leftVal, rightVal)
--- src/test/scala/EvalTest.scala
+++ src/test/scala/EvalTest.scala
@@ -1,27 +1,34 @@
 import org.scalatest.FunSuite
 import tokenizer.tokenizer.tokenize
 import parser.parser.{parseExp, parseSum}
-import eval.eval.eval
+import eval.eval.initEval
 import parser.ast.{Asterisk, GreaterThan, IfExp, InfixExp, IntVal, Minus, Plus}
 
 class EvalTest extends FunSuite {
   test("eval") {
-    assert(eval(parseSum(tokenize("1+2-3"))._1) === IntVal(0))
-    assert(eval(parseSum(tokenize("1 + 2 * 3"))._1) === IntVal(7))
-    assert(eval(parseSum(tokenize("1 + (2 * (3 - 4)) - 5"))._1) === IntVal(-6))
-    assert(eval(parseSum(tokenize("5 * (1 - 2 + (3-4) * 5 -7 * (11 + 1) -6) + 9 * (1 + 3)"))._1) === IntVal(-444))
-    assert(eval(parseSum(tokenize("3 * -2"))._1) === IntVal(-6))
-    assert(eval(parseSum(tokenize("-1 + 2"))._1) === IntVal(1))
-    assert(eval(parseSum(tokenize("3 + -2"))._1) === IntVal(1))
-    assert(eval(parseSum(tokenize("-1 * 5 + -3* 5 + (-10 * 3 * -2 + (1 - 1* 1 - -1) * 5) + 6 -(1 + 4)"))._1) ===IntVal(46))
-    assert(eval(parseSum(tokenize("-10 / 5)"))._1) === IntVal(-2))
-    assert(eval(parseSum(tokenize("10 / -(2*3 -1))"))._1) === IntVal(-2))
-    assert(eval(parseSum(tokenize("8 - 2 - 3"))._1) === IntVal(3))
+    assert(initEval(parseSum(tokenize("1+2-3"))._1) === IntVal(0))
+    assert(initEval(parseSum(tokenize("1 + 2 * 3"))._1) === IntVal(7))
+    assert(initEval(parseSum(tokenize("1 + (2 * (3 - 4)) - 5"))._1) === IntVal(-6))
+    assert(initEval(parseSum(tokenize("5 * (1 - 2 + (3-4) * 5 -7 * (11 + 1) -6) + 9 * (1 + 3)"))._1) === IntVal(-444))
+    assert(initEval(parseSum(tokenize("3 * -2"))._1) === IntVal(-6))
+    assert(initEval(parseSum(tokenize("-1 + 2"))._1) === IntVal(1))
+    assert(initEval(parseSum(tokenize("3 + -2"))._1) === IntVal(1))
+    assert(initEval(parseSum(tokenize("-1 * 5 + -3* 5 + (-10 * 3 * -2 + (1 - 1* 1 - -1) * 5) + 6 -(1 + 4)"))._1) ===IntVal(46))
+    assert(initEval(parseSum(tokenize("-10 / 5)"))._1) === IntVal(-2))
+    assert(initEval(parseSum(tokenize("10 / -(2*3 -1))"))._1) === IntVal(-2))
+    assert(initEval(parseSum(tokenize("8 - 2 - 3"))._1) === IntVal(3))
   }
 
   test("evalIf") {
-    assert(eval(parseExp(tokenize("if 1 > 0 then 1 else 0"))._1) === IntVal(1))
-    assert(eval(parseExp(tokenize("(if (1 * 2) + 3 > -1 then 1 else 0) + 1"))._1) === IntVal(2))
-    assert(parseExp(tokenize("if 1 > 0 then 1 else 0 + 1")) === (IfExp(InfixExp(IntVal(1), GreaterThan, IntVal(0)), IntVal(1), InfixExp(IntVal(0), Plus, IntVal(1))), List()))
+    assert(initEval(parseExp(tokenize("if 1 > 0 then 1 else 0"))._1) === IntVal(1))
+    assert(initEval(parseExp(tokenize("(if (1 * 2) + 3 > -1 then 1 else 0) + 1"))._1) === IntVal(2))
+    assert(initEval(parseExp(tokenize("if 1 > 0 then 1 else 0 + 1"))._1) === IntVal(1))
+  }
+
+
+  test("let") {
+    assert(initEval(parseExp(tokenize("let x = 1 in x + 1"))._1) === IntVal(2))
+    assert(initEval(parseExp(tokenize("let x = 1 in let y = 2 in x + y * 2"))._1) === IntVal(5))
+    assert(initEval(parseExp(tokenize("(let x = 1 in let y = 2 in x + y * 2) * 2"))._1) === IntVal(10))
   }
 }
